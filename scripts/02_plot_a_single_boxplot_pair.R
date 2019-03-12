f4b028aac7b4d1124234d7dc8733734d8c31dd84
Anqi-Dai <adai@bu.edu>
2019-03-12 16:27:08 -0400

    the boxplot is done except the negative strand and the goddamn negative number

---
+++ scripts/02_plot_a_single_boxplot_pair.R
@@ -0,0 +1,51 @@
+## Per-bin boxplots
+# make this a plotting a function
+library(tidyverse)
+
+draw_per_bin_boxpot <- function(TE_name, Num) {
+  # the input should be 
+  box_pos <- DF %>%
+    filter(TE == TE_name) %>%
+    dplyr::select(bin, cnt = str_glue('Pos{Num}'))
+  
+  box_neg <- DF %>%
+    filter(TE == TE_name) %>%
+    dplyr::select(bin, cnt = str_glue('Neg{Num}'))
+  
+  # get the max number of the two matrix that will be the max value for the two heatmaps
+  max_cnt <- max(max(box_pos$cnt), max(box_neg$cnt)) 
+  
+  
+  # plot the TPM of each bin as a box plot. the y limit should be 0 to max count too
+  # the postitive strand
+  pos_box <- ggboxplot(box_pos, x = 'bin', y = 'cnt',
+                       color = '#00468B',
+                       xlab = 'Bin', ylab = 'Positive', 
+                       title = if_else(Num == 1, str_glue('{TE_name} length 18-23nt per-bin boxplots'), if_else(Num == 2, str_glue('{TE_name} length 24-35nt per-bin boxplots'), str_glue('{TE_name} any length per-bin boxplots')))) +
+    ylim(0, max_cnt) +
+    theme_bw() +
+    theme(axis.title.x=element_blank()) +
+    theme(text = element_text(size=25)) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10))
+  
+  # the negative strand
+  neg_box <- ggboxplot(box_neg, x = 'bin', y = 'cnt',
+                       color = '#00468B',
+                       xlab = 'Bin', ylab = 'Negative' )  +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    scale_y_reverse()  + 
+    ylim(max_cnt, 0) +
+    theme_bw() +
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          text = element_text(size=25),
+          axis.ticks.x=element_blank()) 
+  
+  # now assemble the two plots together in one plot
+  ggarrange(pos_box, neg_box, 
+            ncol = 1, nrow = 2) +
+    ggsave(str_glue('../figs/{TE_name}_boxplot_pair_{Num}.jpg'), width = 20, height = 12, dpi = 300)
+}
+
+
+
