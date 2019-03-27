ea41b2e74fc7df185abe12ce2babb11841f9b49c
Anqi-Dai <adai@bu.edu>
2019-03-27 14:22:01 -0400

    finally git  add .git  add .git  add .git  add .git  add .git  add .git  add .git  add .! Ive cleared the lineplot function git  add .git  add .git  add .git  add .git  add .git  add .git  add .

--- scripts/03_plot_a_single_lineplot_pair.R
+++ scripts/03_plot_a_single_lineplot_pair.R
@@ -3,154 +3,147 @@
 library(tidyverse)
 library(ggpubr)
 library(MASS)
-# HERV49I:1-6331
 
-draw_differential_line_plot <- function(TE_name, Num) {
-
-  # the annot table with the group info of the samples
+# the function to generate the table for plotting since the pos and neg have many things in common
+prepare_table_for_plotting <- function(TE_name, StrandNum) {
+  DF <- Test
+  TE_name = 'ALU:1-312'
+  StrandNum = 'Pos1'
+  
+  all_table <- DF %>%
+    filter(TE == TE_name) %>%
+    dplyr::select(Sample, Status, bin, cnt = StrandNum)
+  
   # the shaded area is the interquantile range
-  
-  DF <- Test %>%
-    filter(TE == 'ALU:1-312' ) %>%
-    dplyr::select( Status, bin, Pos1, Neg1)
-  
-
-  line_pos <- DF %>%
-    dplyr::select(-Neg1,  cnt = Pos1) %>%
+  ribbon_part <- all_table %>%
+    dplyr::select(-Sample)  %>%
     group_by(Status, bin) %>%
     summarise(Median = median(cnt),
               quantile1 = quantile(cnt, 0.25),
               quantile3 = quantile(cnt, 0.75))
-
   
-  line_neg <- negative %>%
-    as.data.frame() %>%
-    rename_all(
-      funs(paste0('b',.))
-    ) %>%
-    rownames_to_column() %>%
-    rename(Sample =rowname) %>%
-    gather(key = bin , value = cnt, b0:b99) %>%
-    left_join(sample_annot, by = 'Sample')  %>%
-    group_by(Group, bin) %>%
-    summarise(Median = median(cnt),
-              quantile1 = quantile(cnt, 0.25),
-              quantile3 = quantile(cnt, 0.75))
+  # the position of the stars should be the max of the two medians and then + 10% of the median of each medians.
+  defined_star_height <- ribbon_part %>%
+    group_by(bin) %>%
+    summarise(star_height = max(Median) ) %>%
+    summarise(star_height = median(star_height)*0.1) %>%
+    pull(star_height)
   
-  # the negative binomial distribution part
+  star_position_part <-  ribbon_part %>%
+    group_by(bin) %>%
+    summarise(star_height = max(Median) + defined_star_height )
   
+  # the regression part
   # it's like counts ~ group kind of glm regression
-  # the pheno label
-  #all.equal(rownames(annot) , colnames(pos))
-  pheno <- annot$Group
-  pos <- as.data.frame(pos)
-  # for positive
-  pos_reg <- apply(pos, 1, function(Row){
+  pheno <- all_table %>%
+    arrange(Sample) %>%
+    distinct(Sample, .keep_all = T) %>%
+    pull(Status)
+  
+  cnt_wide <-  all_table %>%
+    dplyr::select(Sample, bin, cnt) %>%
+    spread(key = Sample, value =  cnt ) %>%
+    column_to_rownames('bin')
+  
+  regression_part <- apply(cnt_wide, 1, function(Row){
     ret = summary(glm.nb(as.numeric(Row) ~ pheno))[[12]][2,4]  
     return(ret)
   }) %>% 
     as.data.frame %>% 
+    mutate(bin = as.numeric(rownames(cnt_wide))) %>%
     rename(pval = names(.)[1]) %>%
-    mutate(padj = p.adjust(pval, method = 'BH'))
-
-
-  # the position of the stars should be the max of the two medians and then + 20.
-  star_position <- line_pos %>%
-    group_by(bin) %>%
-    summarise(star_height = max(Median) + 50)
-  
-  star_position_neg <- line_neg %>%
-    group_by(bin) %>%
-    summarise(star_height = max(Median) + 50)
-  
-  # the bins that should annotate with the star signs for the significance level
-  pos_reg <- pos_reg %>%
+    mutate(padj = p.adjust(pval, method = 'BH')) %>%
     mutate(significance = if_else(padj < 0.01, '***', ifelse(padj < 0.05, '**', if_else(padj < 0.1, '*', '')))) %>%
-    mutate(bin = paste0('b', seq(0,99)))%>%
-    full_join(star_position , by = 'bin')
+    full_join(star_position_part , by = 'bin')
   
-  neg_reg <- neg_reg %>%
-    mutate(significance = if_else(padj < 0.01, '***', ifelse(padj < 0.05, '**', if_else(padj < 0.1, '*', '')))) %>%
-    mutate(bin = paste0('b', seq(0,99))) %>%
-    full_join(star_position_neg , by = 'bin')
+  # add the regression part to the ribbon part
+  final <- ribbon_part %>% 
+    left_join(regression_part %>%
+                dplyr::select(significance, star_height, bin), by = 'bin') 
+  return(final)
+}
 
-  # add the significance to the line_pos table
-  Dline_pos <- line_pos %>% 
-    left_join(pos_reg %>%
-                dplyr::select(significance, star_height, bin), by = 'bin') %>%
-    mutate(bin = str_replace_all(bin, '^b','')) %>%
-    mutate(bin = as.numeric(bin)) %>%
-    arrange(bin)
+
+# the actual function to draw the plot
+draw_differential_line_plot <- function(TE_name, Num) {
   
-  Dline_neg <- line_neg %>% 
-    left_join(neg_reg %>%
-                dplyr::select(significance, star_height, bin), by = 'bin')%>%
-    mutate(bin = str_replace_all(bin, '^b','')) %>%
-    mutate(bin = as.numeric(bin)) %>%
-    arrange(bin)
+  DF <- Test
+  TE_name = 'ALU:1-312'
   
+  POS_table <- prepare_table_for_plotting(TE_name, 'Pos1')
+  NEG_table <- prepare_table_for_plotting(TE_name, 'Neg1')
+
+  # the max_cnt, the scale of the plot should be the max of the two quantile 3 times 1.2
+  max_q3 <-max(max(POS_table$quantile3), max(NEG_table$quantile3))
+  max_cnt <-  max_q3 * 1.2
 
   # make the differential line plot
   # for positive
-  dl_pos <- ggplot(Dline_pos) + 
+  dl_pos <- ggplot(POS_table) + 
     geom_ribbon(aes(x=bin,
                     ymin=quantile1,
                     ymax=quantile3,
-                    group=Group),alpha=0.40,fill="#3985ff") + 
+                    group=Status, 
+                    fill= Status ),alpha=0.40) + 
     geom_line(aes(x=bin,
                   y=Median,
-                  group=Group,
-                  color=Group),lwd=1) +
+                  group=Status,
+                  color=Status),lwd=1) +
     geom_point(aes(x=bin,
                    y=Median,
-                   group=Group,
-                   color=Group)) +
+                   group=Status,
+                   color=Status)) +
     geom_text(aes(x=bin, y=star_height, label = significance),angle = 90, nudge_x = 0.2) +
     ylim(0, max_cnt) + 
     labs(x = 'Bin',
          y = 'Positive',
          title = 'Differential lineplot') +
     theme_bw() +
-    scale_x_continuous( breaks = seq(0,99)) +
+    scale_x_continuous( breaks = POS_table$bin) +
     scale_color_manual(values = c('#00468B', '#EC0000'))  +
-    theme(legend.position="none") +
+    scale_fill_manual(values = c('#00468B', '#EC0000'))  +
+    theme(legend.position='top', 
+          legend.justification='right',
+          legend.direction='horizontal') +
     theme(axis.title.x=element_blank()) +
     theme(text = element_text(size=25)) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10)) 
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10))
   
   # negative strand
-  dl_neg <- ggplot(Dline_neg) + 
+  dl_neg <- ggplot(NEG_table) + 
     geom_ribbon(aes(x=bin,
                     ymin=quantile1,
                     ymax=quantile3,
-                    group=Group),alpha=0.40,fill="#3985ff") + 
+                    group=Status, 
+                    fill= Status ),alpha=0.40) + 
     geom_line(aes(x=bin,
                   y=Median,
-                  group=Group,
-                  color=Group),lwd=1) +
+                  group=Status,
+                  color=Status),lwd=1) +
     geom_point(aes(x=bin,
                    y=Median,
-                   group=Group,
-                   color=Group)) +
+                   group=Status,
+                   color=Status)) +
     geom_text(aes(x=bin, y=star_height, label = significance),angle = 90, nudge_x = 0.2) +
-    ylim(0, max_cnt) + 
     labs(x = 'Bin',
          y = 'Negative') +
     theme_bw() +
     scale_color_manual(values = c('#00468B', '#EC0000'))  +
+    scale_fill_manual(values = c('#00468B', '#EC0000'))  +
     scale_y_reverse()  + 
     ylim(max_cnt, 0) +
-    scale_x_continuous( breaks = seq(0,99)) +
-    theme(legend.position="bottom") +
+    scale_x_continuous( breaks = NEG_table$bin)  +
     theme(axis.title.x=element_blank(),
           axis.ticks.x=element_blank(),
           text = element_text(size=25),
-          axis.text.x=element_blank()) 
+          legend.position='none', 
+          axis.text.x=element_blank())  
+   
   
   
   # now assemble the two plots together in one plot
   ggarrange(dl_pos, dl_neg, 
             ncol = 1, nrow = 2) +
-    ggsave('../figs/assembled_lineplot.jpg', width = 20, height = 12, dpi = 300)
+    ggsave('figs/assembled_lineplot.jpg', width = 20, height = 12, dpi = 300)
   
 }
